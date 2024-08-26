094cddcdee965b750501f80680aab82ae93e4681
Tinusha Priyamanka <153253769+tinusha-priyamanka-99@users.noreply.github.com>
2024-08-27 01:05:12 +0530

    second commit

--- utils.py
+++ utils.py
@@ -1,2 +1,25 @@
 import whisper
 from langchain_openai import OpenAI
+from langchain.agents import initialize_agent
+from langchain.agents.agent_toolkits import ZapierToolkit
+from langchain.utilities.zapier import ZapierNLAWrapper
+import os
+
+openai_api_key = os.environ.get["OPENAI_API_KEY"]
+zapier_nla_api_key = os.environ.get["ZAPIER_NLA_API_KEY"]
+
+def email_summary(file):
+
+    llm = OpenAI(temperature=0)
+
+    zapier = ZapierNLAWrapper()
+    toolkit = ZapierToolkit.from_zapier_nla_wrapper(zapier)
+
+    agent = initialize_agent(toolkit.get_tools(),
+                             llm,
+                             agent='zero-shot-react-description',
+                             verbose = True)
+    
+    model = whisper.load_model("base")
+
+    re
